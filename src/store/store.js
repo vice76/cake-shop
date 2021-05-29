d58e757483d2311a701174f3d06c6c9f393f66c4
vice76 <arpityadavaj1999@gmail.com>
2021-05-29 15:40:13 +0530

    added composite devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore , applyMiddleware} from "redux";
 import rootReducer from "./reducer/rootReducer";
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer  , composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
